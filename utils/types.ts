6af769cc3fb464ed54c0fe46c99209443fc7c2d5
= <=>
2024-08-14 16:22:47 +0500

    commit

--- utils/types.ts
+++ utils/types.ts
@@ -1,15 +1,13 @@
-
-export type actionFunction =(
-    prevState:any, 
-    formData:FormData) 
-    =>Promise<{message:string}>;
+export type actionFunction = (
+  prevState: any,
+  formData: FormData
+) => Promise<{ message: string }>;
 
 export type PropertyCardProps = {
-    image: string;
-    id: string;
-    name: string;
-    tagline: string;
-    country: string;
-    price: number;
-    };
-
+  image: string;
+  id: string;
+  name: string;
+  tagline: string;
+  country: string;
+  price: number;
+};
